44570ac0fa4e27819f53c3d610c6ba0f99d5f4f2
Mehdi GRICHE <mehdigriche@gmail.com>
2019-07-04 16:25:55 +0100

    Initial commit

---
+++ app/src/main/java/com/mehdigriche/chatapp/UserDetails.java
@@ -0,0 +1,11 @@
+package com.mehdigriche.chatapp;
+
+/**
+ * Created by Mehdi GRICHE on 04/07/2019.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
